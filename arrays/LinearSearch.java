3f4be092fec75948b7c805ac3be12bbfd4515a40
mirjaj@ <mirjajajijmilon@gmail.com>
2026-06-12 15:49:11 +0530

    Solve Linear Search

--- arrays/LinearSearch.java
+++ arrays/LinearSearch.java
@@ -1,8 +1,17 @@
 package arrays;
 
-public class LinearSearch {
+public class LinearSearch{
 
-    public static void main(String[] args) {
+   public static void linear_search(int arr[], int key){
+      for(int i=0; i<arr.length; i++){
+          if(arr[i] == key){
+             System.out.print("Element Found at index : " + i);
+          }
+      }
+   }
+   public static void main(String args[]){
 
-    }
+     int arr[] = {5,7,89,6,23,5};
+     linear_search(arr, 6); 
+   }
 }
